247e5f33e86173f6e7bbe9027ab0eabacbdc6037
BFlorian91 <florianbeaumont412.com>
2022-04-27 16:31:39 +0200

    back: clean api folder

--- pages/api/users.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const { file } = req.body
-  console.log(req.body);
-
-
-  res.status(200).json({ name: file })
-}
